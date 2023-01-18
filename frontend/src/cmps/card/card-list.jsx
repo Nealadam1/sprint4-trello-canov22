e8d6e4a57cc088f0a3ae557c2ef1c9eb5330feb4
Gilad Dagan <giladagan53@gmail.com>
2023-01-18 19:50:41 +0200

    basic styling for card preview

--- frontend/src/cmps/card/card-list.jsx
+++ frontend/src/cmps/card/card-list.jsx
@@ -5,7 +5,7 @@ import { utilService } from "../../services/util.service"
 import { setBoard, updateBoard } from "../../store/actions/board.action"
 import { CardPreview } from "./card-preview"
 
-export function CardList({ cards, group, groups }) {
+export function CardList({ group, groups }) {
   let currBoard = useSelector((storeState) => storeState.boardModule.board)
 
   function onAddCard() {
@@ -14,8 +14,18 @@ export function CardList({ cards, group, groups }) {
       id: utilService.makeId(),
       title,
     }
-    cards = [...cards, newCard]
-    const updatedGroup = { ...group, cards }
+    group.cards = [...group.cards, newCard]
+    const updatedGroup = { ...group, cards: group.cards }
+    const updatedGroups = groups.map((group) =>
+      group.id === updatedGroup.id ? updatedGroup : group
+    )
+    setBoard({ ...currBoard, groups: updatedGroups })
+    updateBoard({ ...currBoard, groups: updatedGroups })
+  }
+
+  function onDeleteCard(cardId) {
+    group.cards = group.cards.filter((card) => card.id !== cardId)
+    const updatedGroup = { ...group, cards: group.cards }
     const updatedGroups = groups.map((group) =>
       group.id === updatedGroup.id ? updatedGroup : group
     )
@@ -26,14 +36,17 @@ export function CardList({ cards, group, groups }) {
   return (
     <>
       <div className="card-list">
-        {cards &&
-          cards.map((card) => (
+        {group.cards &&
+          group.cards.map((card) => (
             <li key={card.id}>
               <CardPreview card={card} />
+              <div className="delete-card-btn">
+                <button onClick={() => onDeleteCard(card.id)}>X</button>
+              </div>
             </li>
           ))}
       </div>
-      <button onClick={onAddCard}>Add card +</button>
+      <button onClick={onAddCard}> + Add card</button>
     </>
   )
 }
