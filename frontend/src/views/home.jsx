e2ee6379b4331badbf02a1d238ace36e080db4fa
Liron Kruchinin <lironkruch@gmail.com>
2023-01-18 10:52:04 +0200

    changes

--- frontend/src/views/home.jsx
+++ frontend/src/views/home.jsx
@@ -1,13 +1,9 @@
 import React from "react"
 import { Link } from "react-router-dom";
 export function Home() {
-<<<<<<< Updated upstream
   return (
     <section>
       <Link to="/board" >Board</Link>
     </section>
   )
-=======
-  return <div>Home</div> 
->>>>>>> Stashed changes
 }
