4f6a1da22659dc31a4a94fdb1be16c519fc5c5fa
Nealadam1 <Nealadam@live.com>
2023-01-19 17:17:13 +0200

    changes to board header

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -8,10 +8,8 @@ import { utilService } from "../services/util.service"
 
 export function AppHeader() {
   const board=useSelector((storeState)=> storeState.boardModule.board)
-  console.log(board)
   const headerBackground=(board)? utilService.darken(board.style.backgroundColor, -40) : ''
-  console.log(headerBackground)
-  
+
   return <div className="app-header" style={{backgroundColor:headerBackground }}>
     <Link to="/"><FontAwesomeIcon className='btn-icon' icon={faTrello} style={{backgroundColor: board?.style.backgroundColor}}/>Trello</Link></div>
 }
