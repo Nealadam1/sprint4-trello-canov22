e52430793eb4af74cc67245862eeecd8271816ba
Liron Kruchinin <lironkruch@gmail.com>
2023-01-20 12:48:58 +0200

    card css

--- frontend/src/cmps/card/card-preview.jsx
+++ frontend/src/cmps/card/card-preview.jsx
@@ -24,21 +24,19 @@ export function CardPreview({ card }) {
       {card?.style?.bgColor ? (
         <header
           className="card-header"
-          style={{
-            backgroundColor: card.style.bgColor,
-            width: "100%",
-            height: "32px",
-          }}
-        ></header>
+          style={{ backgroundColor: card.style.bgColor }}>
+        </header>
       ) : null}
 
-      {card?.labelIds ? card.labelIds.map((label) => label) : ""}
-      <p>{card.title}</p>
+      <div className="card-info">
+        {card?.labelIds ? card.labelIds.map((label) => label) : ""}
+        <p>{card.title}</p>
+        <div className="member-images">
+          {currMembers.map((member) => (
+            <img className="member-image" alt="" src={member.imgUrl} />
+          ))}
+        </div>
 
-      <div className="member-images">
-        {currMembers.map((member) => (
-          <img className="member-image" alt="" src={member.imgUrl} />
-        ))}
       </div>
     </div>
   )
