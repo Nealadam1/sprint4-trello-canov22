c750ae278d7b5923d071209c8228ab2a5ef42d63
Gilad Dagan <giladagan53@gmail.com>
2023-01-21 22:50:06 +0200

    sync

--- frontend/src/cmps/group/group-preview.jsx
+++ frontend/src/cmps/group/group-preview.jsx
@@ -1,22 +1,36 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import { CardList } from "../card/card-list"
 
-export function GroupPreview({ group, cards }) {
+export function GroupPreview({ group, cards, updateGroupTitle }) {
   const [groupTitleToInput, setGroupTitleToInput] = useState({})
+  const [newTitle, setNewTitle] = useState(group.title)
+  const inputRef = useRef(null)
+
+  const handleBlur = (event) => {
+    updateGroupTitle(group.id, event.target.value)
+    setGroupTitleToInput({ ...groupTitleToInput, [group.id]: false })
+  }
 
   return (
     <div className="group-preview">
       {groupTitleToInput[group.id] ? (
         <form>
-          <input type="text" />
+          <input
+            type="text"
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+            onBlur={handleBlur}
+            ref={inputRef}
+          />
         </form>
       ) : (
         <h4
-          onClick={() =>
+          onClick={() => {
             setGroupTitleToInput({ ...groupTitleToInput, [group.id]: true })
-          }
+            inputRef.current.focus()
+          }}
         >
-          {group.title}
+          {newTitle}
         </h4>
       )}
       <CardList cards={cards} group={group} />
