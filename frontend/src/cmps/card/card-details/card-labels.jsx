509e484897ba6b679d745c062fee685c78ddea8a
Gilad Dagan <giladagan53@gmail.com>
2023-01-22 20:32:01 +0200

    fixed labels in card details

--- frontend/src/cmps/card/card-details/card-labels.jsx
+++ frontend/src/cmps/card/card-details/card-labels.jsx
@@ -1,23 +1,29 @@
 import { useSelector } from "react-redux"
 import { utilService } from "../../../services/util.service"
 
-export function CardLabels({ CardLabels }) {
+export function CardLabels({ cardLabels }) {
   const displayLabels = []
 
   const labels = useSelector((storeState) => storeState.labelModule.labels)
 
   return (
     <div className="card-labels">
-      {CardLabels?.map((label) => {
+      {cardLabels?.map((label) => {
         labels.map((displayLabel) => {
           if (label === displayLabel.id) displayLabels.push(displayLabel)
         })
       })}
-
       {displayLabels.map((label) => (
-        <div className="card-label" key={label.id} style={{ background: utilService.changeContrast(label.color) }}>
-          <div className="circle" style={{ background: label.color }}></div>{label.title}
-        </div>
+        <span
+          className="card-label"
+          key={label.id}
+          style={{
+            background: utilService.lightenColor(label.color),
+          }}
+        >
+          <div className="circle" style={{ background: label.color }}></div>
+          {label.title}
+        </span>
       ))}
     </div>
   )
