ad296593be06dabc1555421d175ec14740c73d4f
Liron Kruchinin <lironkruch@gmail.com>
2023-01-25 19:09:33 +0200

    fixed login system, changed css

--- backend/api/board/board.service.js
+++ backend/api/board/board.service.js
@@ -19,7 +19,7 @@ async function getById(boardId) {
     try {
         const collection = await dbGetCollection
         const board = await collection.findOne({ _id: ObjectId(boardId) })
-        console.log(board);
+        // console.log(board);
         return board
     } catch (err) {
         throw err
@@ -38,7 +38,7 @@ async function add(board) {
 
 async function remove(boardId) {
     try {
-        console.log(boardId);
+        // console.log(boardId);
         const collection = await dbGetCollection
         await collection.deleteOne({ _id: ObjectId(boardId) })
         return boardId
@@ -49,9 +49,9 @@ async function remove(boardId) {
 
 async function update(board) {
     try {
-        console.log('board');
-        console.log(board);
-        console.log();
+        // console.log('board');
+        // console.log(board);
+        // console.log();
         const boardToSave = {
             title: board.title,
             isStarred: board.isStarred,
@@ -62,7 +62,7 @@ async function update(board) {
             groups: board.groups,
             activities: board.activities
         }
-        console.log(boardToSave);
+        // console.log(boardToSave);
 
         const collection = await dbGetCollection
         await collection.updateOne({ _id: ObjectId(board._id) }, { $set: boardToSave })
