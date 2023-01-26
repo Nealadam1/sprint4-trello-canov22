82035cf6db0ecc5eecf99331a4aee9cfc1f116a0
Liron Kruchinin <lironkruch@gmail.com>
2023-01-26 19:37:23 +0200

    fixed image upload bug

--- frontend/src/cmps/card/card-activities.jsx
+++ frontend/src/cmps/card/card-activities.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { useSelector } from "react-redux"
 import { utilService } from "../../services/util.service"
+import { CardChangeBg } from "./card-preview/card-changeBg"
 
 export function CardActivites() {
   const modalRef = useRef(null)
-
+  const [isEditing, setIsEditing] = useState(false)
   const board = useSelector((storeState) => storeState.boardModule.board)
 
   // const oneHourAgo = Date.now() - (60 * 60 * 1000);
@@ -17,32 +18,36 @@ export function CardActivites() {
   }, [board.activities.length])
   return (
     <div ref={modalRef} className="card-activites">
-      <p className="activities-title">Activites</p>
-      <div className="sep-line"></div>
-      <ul>
-        {board.activities.map((activitie) => {
-          // console.log(activitie)
-          return (
-            <li className="activitie" key={activitie.id}>
-              <div className="user-activitie-img">
-                <img src={activitie.userImage} />
-              </div>
-              <div className="user-activitie-info">
-                <p>
-                  {activitie.fullname +
-                    " " +
-                    activitie.data +
-                    " " +
-                    activitie.text}
-                </p>
-                <span className="activitie-time">
-                  {utilService.formatTime(activitie.createdAt)}
-                </span>
-              </div>
-            </li>
-          )
-        })}
-      </ul>
+      <CardChangeBg />
+      {!isEditing && <div>
+        <p className="activities-title">Activites</p>
+        <div className="sep-line"></div>
+        <ul>
+          {board.activities.map((activitie) => {
+            // console.log(activitie)
+            return (
+              <li className="activitie" key={activitie.id}>
+                <div className="user-activitie-img">
+                  <img src={activitie.userImage} />
+                </div>
+                <div className="user-activitie-info">
+                  <p>
+                    {activitie.fullname +
+                      " " +
+                      activitie.data +
+                      " " +
+                      activitie.text}
+                  </p>
+                  <span className="activitie-time">
+                    {utilService.formatTime(activitie.createdAt)}
+                  </span>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>}
+
     </div>
   )
 }
---
+++ frontend/src/cmps/card/card-preview/card-changeBg.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function CardChangeBg() {
+    return (
+        <section>
+            <button className="grey-back">Change background</button>
+        </section>
+    )
+}
