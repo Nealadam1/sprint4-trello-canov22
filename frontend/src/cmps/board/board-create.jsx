1a115f2440a4a57b3662723ec5ba267a03e05ef4
Nealadam1 <Nealadam@live.com>
2023-01-26 15:52:31 +0200

    changed color pallete for labels and cover

--- frontend/src/cmps/board/board-create.jsx
+++ frontend/src/cmps/board/board-create.jsx
@@ -90,7 +90,7 @@ export function CreateBoard() {
       </div>
       <div className="background-picker">
         <h5>Background</h5>
-        <TwitterPicker
+        <TwitterPicker colors={['#7BC86C', '#F5DD29','#FFAF3F','#EF7564','#CD8DE5','#5BA4CF','#29CCE5','#6DECA9','#FF8ED4','#172B4D']}
           color={boardPreviewColor}
           onChange={handleBackgroundChange}
         />
